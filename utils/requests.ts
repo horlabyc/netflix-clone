8a6adfbe0f15c5ecdd69ac5aea324f3b9945adac
Olaosebikan Sulaiman <olabisisulaiman@gmail.com>
2023-02-21 12:07:52 +0100

    Banner component done

--- utils/requests.ts
+++ utils/requests.ts
@@ -2,7 +2,7 @@ const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
 
 const requests = {
-  fetchTrending: `${TMDB_BASE_URL}/trending/all/week?api_key=${TMDB_API_KEY}$language=en-US`,
+  fetchTrending: `${TMDB_BASE_URL}/trending/all/week?api_key=${TMDB_API_KEY}&language=en-US`,
   fetchNetflixOriginals: `${TMDB_BASE_URL}/discover/movie?api_key=${TMDB_API_KEY}&with_networks=213`,
   fetchTopRated: `${TMDB_BASE_URL}/movie/top_rated?api_key=${TMDB_API_KEY}&language=en-US`,
   fetchActionMovies: `${TMDB_BASE_URL}/discover/movie?api_key=${TMDB_API_KEY}&language=en-US&with_genres=28`,
